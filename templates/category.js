8bd4c670eca35b61c5990003d30d5fc876c2f96a
iandioch <iandioch11@gmail.com>
2015-12-23 04:44:18 +0000

    Fix bug where links from category and tag pages were incorrect

--- templates/category.js
+++ templates/category.js
@@ -3,7 +3,7 @@ echo("<html><head><meta charset=\"utf-8\"/><title>Noah's Blog</title></head>");
 echo("<body>");
 echo("<ul>");
 for(var i = posts.length-1; i > posts.length-11 && i >= 0; i --){
-	echo("<li><a href=\"" + posts[i].Location + "\">" + posts[i].Meta.Title + "</a></li>");
+	echo("<li><a href=\"../" + posts[i].Location + "\">" + posts[i].Meta.Title + "</a></li>");
 }
 echo("</ul>")
 echo("</body>");
