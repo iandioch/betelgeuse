9402762ef9c1ffce88cb6fb8178880ee79176e7d
iandioch <iandioch11@gmail.com>
2015-12-23 05:00:27 +0000

    Add category and tag data of each post to index page

--- templates/index.js
+++ templates/index.js
@@ -5,7 +5,40 @@ echo("<h1>Noah's Blog</h1>");
 echo("<p>Recent posts:</p>");
 echo("<ul>");
 for(var i = posts.length-1; i > posts.length-11 && i >= 0; i --){
-	echo("<li><a href=\"" + posts[i].Location + "\">" + posts[i].Meta.Title + "</a></li>");
+	echo("<li>");
+
+	echo("<h5><a href=\"" + posts[i].Location + "\">" + posts[i].Meta.Title + "</a></h5>");
+	echo("<ul>")
+	var cats = posts[i].Meta.Categories;
+	if (cats.length > 0) {
+
+		echo("<li>Categories: <a href=\"./categories/" + cats[0] + ".html\">" + cats[0] + "</a>");
+
+		var cats = posts[i].Meta.Categories;
+		for (var j = 1; j < cats.length; j ++){
+			//echo(posts[j].Meta.Categories);
+			echo(", <a href=\"./categories/" + cats[j] + ".html\">" + cats[j] + "</a>");
+			//echo("lol");
+		}
+		echo("</li>");
+	}
+
+	var tags = posts[i].Meta.Tags;
+	if (tags.length > 0) {
+
+		echo("<li>Tags: <a href=\"./tags/" + tags[0] + ".html\">" + tags[0] + "</a>");
+
+		var cats = posts[i].Meta.Categories;
+		for (var j = 1; j < tags.length; j ++){
+			//echo(posts[j].Meta.Categories);
+			echo(", <a href=\"./tags/" + tags[j] + ".html\">" + tags[j] + "</a>");
+			//echo("lol");
+		}
+		echo("</li>");
+	}
+	
+	echo("</ul>");
+	echo("</li>");
 }
 echo("</ul>")
 echo("</body>");
