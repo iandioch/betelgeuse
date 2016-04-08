b609622691f3ead0971e18db177f080c3255f5e3
iandioch <iandioch11@gmail.com>
2016-04-08 04:00:30 +0100

    Update look of the site

--- templates/index.js
+++ templates/index.js
@@ -1,19 +1,20 @@
 echo("<!doctype HTML>")
 echo("<html><head><meta charset=\"utf-8\"/><title>Noah Donnelly</title><link href='https://fonts.googleapis.com/css?family=Slabo+27px&subset=latin,latin-ext' rel='stylesheet' type='text/css'><link rel=\"stylesheet\" type=\"text/css\" href=\"styles/stylesheet.css\"></head>");
 echo("<body>");
-echo("<div class=\"title\"><h1>Noah \"The Rock\" Donnelly</h1><p>Hi, I'm Noah Donnelly, a programmer from Monaghan, Ireland. I'm studying <a href=\"tags/cpssd.html\">Computational Problem Solving & Software Development</a> in <a href=\"http://dcu.ie\">Dublin City University</a>. I'm the current secretary of <a href=\"http://redbrick.dcu.ie\">Redbrick</a>, DCU's Networking Society. I'm into languages, competitive programming, and poor films. See my CV <a href=\"https://docs.google.com/document/d/1Nl6RUxY6QRPBq0aYQ85JeKBV4yBSr4X-rNM6PbhoVP4/\">here</a>. Read about my <a href=\"categories/projects.html\">projects</a>, check out my <a href=\"http://github.com/iandioch\">Github</a>, and follow me on <a href=\"http://twitter.com/iandioch\">Twitter</a>.<br><a href=\"rss\">RSS</a></p></div>");
+echo("<div class=\"title\"><h1>Noah Donnelly</h1><p>Hi, I'm Noah Donnelly, a programmer from Monaghan, Ireland. I'm studying <a href=\"tags/cpssd.html\">Computational Problem Solving & Software Development</a> in <a href=\"http://dcu.ie\">Dublin City University</a>. I'm the current secretary of <a href=\"http://redbrick.dcu.ie\">Redbrick</a>, DCU's Networking Society. I'm into languages, competitive programming, and poor films. See my CV <a href=\"https://docs.google.com/document/d/1Nl6RUxY6QRPBq0aYQ85JeKBV4yBSr4X-rNM6PbhoVP4/\">here</a>. Read about my <a href=\"categories/projects.html\">projects</a>, check out my <a href=\"http://github.com/iandioch\">Github</a>, and follow me on <a href=\"http://twitter.com/iandioch\">Twitter</a></p></div>");
 echo("<div class=\"page-content\">");
 echo("<p>Recent posts:</p>");
-echo("<ul>");
+//echo("<ul>");
 for(var i = posts.length-1; i > posts.length-11 && i >= 0; i --){
-	echo("<li>");
+	//echo("<li>");
 
-	echo("<h5><a href=\"" + posts[i].Location + "\">" + posts[i].Meta.Title + "</a></h5>");
-	echo("<ul>")
+	echo("<h2><a href=\"" + posts[i].Location + "\">" + posts[i].Meta.Title + "</a></h2>");
+	//echo("<ul>")
 	var cats = posts[i].Meta.Categories;
 	if (cats.length > 0) {
 
-		echo("<li>Categories: <a href=\"./categories/" + cats[0] + ".html\">" + cats[0] + "</a>");
+		//echo("<li>Categories: <a href=\"./categories/" + cats[0] + ".html\">" + cats[0] + "</a>");
+        echo("<p>Categories: <a href=\"./categories/" + cats[0] + ".html\">" + cats[0] + "</a>");
 
 		var cats = posts[i].Meta.Categories;
 		for (var j = 1; j < cats.length; j ++){
@@ -21,13 +22,15 @@ for(var i = posts.length-1; i > posts.length-11 && i >= 0; i --){
 			echo(", <a href=\"./categories/" + cats[j] + ".html\">" + cats[j] + "</a>");
 			//echo("lol");
 		}
-		echo("</li>");
+        echo("</p>");
+		//echo("</li>");
 	}
 
 	var tags = posts[i].Meta.Tags;
 	if (tags.length > 0) {
 
-		echo("<li>Tags: <a href=\"./tags/" + tags[0] + ".html\">" + tags[0] + "</a>");
+		//echo("<li>Tags: <a href=\"./tags/" + tags[0] + ".html\">" + tags[0] + "</a>");
+        echo("<p>Tags: <a href=\"./categories/" + tags[0] + ".html\">" + tags[0] + "</a>");
 
 		var cats = posts[i].Meta.Categories;
 		for (var j = 1; j < tags.length; j ++){
@@ -35,12 +38,16 @@ for(var i = posts.length-1; i > posts.length-11 && i >= 0; i --){
 			echo(", <a href=\"./tags/" + tags[j] + ".html\">" + tags[j] + "</a>");
 			//echo("lol");
 		}
-		echo("</li>");
+        echo("</p>");
+		//echo("</li>");
 	}
+    echo("<br>");
 	
-	echo("</ul>");
-	echo("</li>");
+	//echo("</ul>");
+	//echo("</li>");
 }
-echo("</ul></div>");
+//echo("</ul></div>");
+echo("</div>");
+echo("<div><p>Subscribe to <a href=\"rss\">my RSS feed</a></p></div>");
 echo("</body>");
 echo("</html>");
